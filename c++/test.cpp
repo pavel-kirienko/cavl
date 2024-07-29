21ee3c89587f0c8856e44e1f709f75e171037125
Sergei Shirokov <sergei.shirokov@zubax.com>
2024-07-29 20:23:43 +0300

    style fix

--- c++/test.cpp
+++ c++/test.cpp
@@ -155,7 +155,8 @@ template <typename T>
 void checkPostOrdering(const N<T>* const root, const std::vector<std::uint16_t>& expected, const bool reverse = false)
 {
     std::vector<std::uint16_t> order;
-    T::postOrderTraverse(root, [&](const N<T>& nd) { order.push_back(nd.getValue()); }, reverse);
+    T::postOrderTraverse(
+        root, [&](const N<T>& nd) { order.push_back(nd.getValue()); }, reverse);
     TEST_ASSERT_EQUAL(expected.size(), order.size());
     if (!order.empty())
     {
