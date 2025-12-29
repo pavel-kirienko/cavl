3254c6f6a4ac8fdce754d1ebcc3cf4b0acd45812
Pavel Kirienko <pavel.kirienko@gmail.com>
2025-12-29 18:21:31 +0200

    Apply suggestion from @pavel-kirienko

--- c/cavl2.h
+++ c/cavl2.h
@@ -164,9 +164,6 @@ static inline bool cavl2_is_inserted(const CAVL2_T* const root, const CAVL2_T* c
 /// Remove the specified node if it is inserted in the tree; otherwise, do nothing.
 /// This is a convenience wrapper that combines cavl2_is_inserted() and cavl2_remove().
 /// Returns true if the node was inserted and has been removed, false otherwise.
-/// The worst-case complexity is O(log n) if the node is inserted, O(1) otherwise.
-/// The function has no effect and returns false if either of the pointers are NULL.
-/// The removed node will have all of its pointers set to NULL.
 static inline bool cavl2_remove_if(CAVL2_T** const root, CAVL2_T* const node)
 {
     bool removed = false;
